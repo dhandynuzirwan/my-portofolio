e290ebe39b112bae832308682df97fe124ca894e
dhandynuzirwan <dhandynuzirwan@gmail.com>
2025-12-15 14:38:33 +0700

    Update basePath in next.config.ts and add gh-pages dependency in package.json

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const basePath = process.env.NODE_ENV === "production" ? "/Resume-Nextjs" : "";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath,
+  basePath: '/my-portofolio',
   assetPrefix: basePath,
   images: {
     unoptimized: true,
